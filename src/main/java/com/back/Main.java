c17d8d6cf5bb2571236bd4384d3c7bab74b1eefa
Code <yna4371@gmail.com>
2025-05-22 17:42:06 +0900

    First commit

---
+++ src/main/java/com/back/Main.java
@@ -0,0 +1,21 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+
+
+        while(true){
+            String wd = sc.next();
+            if(wd.equals("종료")){
+                System.out.println("명령) "+ wd);
+                return;
+            }
+        }
+
+    }
+}
